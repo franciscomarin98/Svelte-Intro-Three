81d20cf8da4fd3c3099bcb5b3fe01d9344deb2d0
franciscomarin98 <francmarinc@gmail.com>
2021-04-04 13:05:54 -0500

    Seguimos dandole caña, ahora aprendiendo como hacer debbug y a su vez aprendiendo otros trucos en Html y pasando props de otra forma

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,22 @@
 <script>
+	//Imports
+	import Button from "./Button.svelte";
 	import Data from "./Data.svelte";
 
-	export let name,
-		lastName = "Marin";
+	//States
 	let myObject = {
 		propiedadOne: "Nickname",
 		propiedadTwo: "Sisco",
+	};
+	let disabled = true;
+
+	//Props
+	export let name,
+		lastName = "Marin";
+
+	export let buttonOptions = {
+		text: "btnComponent",
+		disabled: false,
 	};
 </script>
 
@@ -15,5 +26,18 @@
 		My name is {name}
 		{lastName}
 	</p>
+	<a href="https://mipagina.com/{name}">Forma 1</a>
+	<a href={`https://mipagina.com/${name}`}>Forma 2</a>
 	<Data {...myObject} />
+	<!-- {console.log(myObject)}
+	{@debug myObject} -->
+	<input type="text" />
+	<button {disabled}>Click</button>
+	<Button {...buttonOptions} />
 </main>
+
+<style>
+	:global(button) {
+		border: 1px solid red;
+	}
+</style>
---
+++ src/Button.svelte
@@ -0,0 +1,7 @@
+<button disabled={$$props.disabled}>{$$props.text}</button>
+
+<style>
+    button {
+        border: 1px solid black;
+    }
+</style>
